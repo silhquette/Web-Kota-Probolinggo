e3b7a69d43dc509c980d8257ff9627e1754a1716
silhquette <muhammadqiyam03@gmail.com>
2024-08-14 23:10:08 +0700

    benerin link

--- resources/js/Pages/Informasi/SetiapSaat.jsx
+++ resources/js/Pages/Informasi/SetiapSaat.jsx
@@ -63,7 +63,7 @@ export default function InformationIndex({ informations }) {
                                                 <td className="py-2 px-4">{item.format}</td>
                                                 <td className="py-2 px-4">{item.announced}</td>
                                                 <td className="py-2 px-4">{item.period}</td>
-                                                <td className="py-2 px-4">{item.link}</td>
+                                                <td className="py-2 px-4 w-28"><a href={item.link} className="text-blue-700 after:content-['_↗']">Kunjungi</a></td>
                                             </tr>
                                         ))}
                                     </>
