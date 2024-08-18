b87d60fe66fa7ffc2cc2fe4c368a758b869303f7
silhquette <muhammadqiyam03@gmail.com>
2024-08-18 16:01:33 +0700

    delete react router

--- resources/js/Pages/Articles/All.jsx
+++ resources/js/Pages/Articles/All.jsx
@@ -1,6 +1,5 @@
 //import React
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 
 //import layout
 import GuestLayout from "../../Layouts/GuestLayout";
